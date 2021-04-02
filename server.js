e4cbd0ae4b0a59d05e6824ea5db8c1332bccc89b
Omar Ibrahim <60094882@cna-qatar.edu.qa>
2021-04-02 05:51:02 +0300

    more await fixes

--- server.js
+++ server.js
@@ -14,89 +14,89 @@ const SECRETKEY = process.env.SECRETKEY
 app.use(express.json())
 app.use(express.static("webfiles"))
 
-(async()=>{
-let con = await mysql.createConnection({
-    host: "localhost",
-    user: "omar",
-    password: PASSWORD,
-    database: DATABASE
-})
-
-app.get("/", (req, res) => {
-    console.log("Connected!")
-    let sql = "select * from users;"
-    await con.query(sql).then(() => {
-        if (err) throw err
-        res.status(200)
-        res.send(result)
-    })
-})
-
-app.post("/api/login", (req, res) => {
-    console.log(req.body)
-    let u = req.body.username
-    let p = req.body.password
-    if (!u || !p) {
-        res.status(400)
-        res.send("Bad Request")
-        return
-    }
-
-    let users = await con.query("select * from users;")
-
-    let user = users.find(user => {
-        console.log("user.password", user.password)
-        console.log("comparison", bcrypt.compareSync(p, user.password))
-        return user.username === u && bcrypt.compareSync(p, user.password)
+(async () => {
+    let con = await mysql.createConnection({
+        host: "localhost",
+        user: "omar",
+        password: PASSWORD,
+        database: DATABASE
     })
 
-    if (user) {
-        let userInfo = {
-            name: u,
-            role: user.role
-        }
-        let token = jwt.sign(userInfo, SECRETKEY)
-        res.send(token)
-        res.status(200)
-        return
-    }
-
-    res.status(401)
-    res.send("not authorized")
-})
-
-app.get("/api/messages", (req, res) => {
-    try {
-        let token = req.headers["authorization"].split(" ")[1]
-        token = jwt.verify(token, SECRETKEY)
-
-        let sql = "select * from users where messages.userid = users.id"
-        con.query(sql, (err, result) => {
+    app.get("/", async (req, res) => {
+        console.log("Connected!")
+        let sql = "select * from users;"
+        await con.query(sql).then(() => {
             if (err) throw err
             res.status(200)
             res.send(result)
         })
+    })
 
-        if (token.name) {
-
-        } else {
-
+    app.post("/api/login", async (req, res) => {
+        console.log(req.body)
+        let u = req.body.username
+        let p = req.body.password
+        if (!u || !p) {
+            res.status(400)
+            res.send("Bad Request")
+            return
         }
 
-        res.send("ok")
-    }
-    catch (e) {
+        let users = await con.query("select * from users;")
+
+        let user = users.find(user => {
+            console.log("user.password", user.password)
+            console.log("comparison", bcrypt.compareSync(p, user.password))
+            return user.username === u && bcrypt.compareSync(p, user.password)
+        })
+
+        if (user) {
+            let userInfo = {
+                name: u,
+                role: user.role
+            }
+            let token = jwt.sign(userInfo, SECRETKEY)
+            res.send(token)
+            res.status(200)
+            return
+        }
+
         res.status(401)
         res.send("not authorized")
-    }
-})
+    })
 
-app.post("/api/logout", (req, res) => {
-    res.send("ok")
-})
+    app.get("/api/messages", async (req, res) => {
+        try {
+            let token = req.headers["authorization"].split(" ")[1]
+            token = jwt.verify(token, SECRETKEY)
+
+            let sql = "select * from users where messages.userid = users.id"
+            con.query(sql, (err, result) => {
+                if (err) throw err
+                res.status(200)
+                res.send(result)
+            })
+
+            if (token.name) {
+
+            } else {
+
+            }
+
+            res.send("ok")
+        }
+        catch (e) {
+            res.status(401)
+            res.send("not authorized")
+        }
+    })
+
+    app.post("/api/logout", (req, res) => {
+        res.send("ok")
+    })
 
 
-app.listen(port, () => {
-    console.log("The application has started")
-})
+    app.listen(port, () => {
+        console.log("The application has started")
+    })
 })()
