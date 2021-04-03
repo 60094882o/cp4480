45793afa29a7d01d891c8521c4e854a6186284bb
Omar Ibrahim <60094882@cna-qatar.edu.qa>
2021-04-03 03:39:06 +0300

    added jest globals for eslint

--- test.test.js
+++ test.test.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv")
 // const mysql = require("mysql")
 dotenv.config()
 
-let expect, test
 
 // const DATABASE = process.env.DATABASE
 // const PASSWORD = process.env.PASSWORD
