579b1a5f8509b8338d9f44c8143166ea6cb5c841
Omar Ibrahim <60094882@cna-qatar.edu.qa>
2021-04-03 05:01:43 +0300

    Await style recommended by jest

--- test.test.js
+++ test.test.js
@@ -15,20 +15,25 @@ let token = null
 
 let params = { Authorization: `Bearer ${token}` }
 
-test("Logging in as user", () => {
+test("Logging in as user", async () => {
 	async function login() {
 		let login = await axios.post(`/${API}/login`, { username: "omar", password: "omartest" })
 		token = login.data
 	}
-	login().then(() => {
-		let isNull = true
-		if (token) isNull = false
-		console.log("token", token)
-		expect(isNull).toBe(false)
-	},console.log)
+	await login()
+	expect(token !== null).toBe(true)
 })
 
-test("Send message as a user", () => {
+test("Logging in as an admin", async () => {
+	async function login() {
+		let login = await axios.post(`/${API}/login`, { username: "kareem", password: "kareemtest" })
+		token = login.data
+	}
+	await login()
+	expect(token !== null).toBe(true)
+})
+
+test("Send message as a user", async () => {
 	let response = null
 	async function sendMessageAsUser() {
 		response = await axios.post("/api/messages", {
@@ -36,38 +41,23 @@ test("Send message as a user", () => {
 			message: "Hello there"
 		}, { params })
 	}
-	sendMessageAsUser().then(() => {
-		expect(response.data).toBe("Message sent")
-	},console.log)
+	await sendMessageAsUser()
+	expect(response.data).toBe("Message sent")
 })
 
-test("Logging in as an admin", () => {
-	async function login() {
-		let login = await axios.post(`/${API}/login`, { username: "kareem", password: "kareemtest" })
-		token = login.data
-	}
-	login().then(() => {
-		let isNull = true
-		if (token) isNull = false
-		expect(isNull).toBe(false)
-	},console.log)
-})
-
-test("Send message as an admin", () => {
+test("Send message as an admin", async () => {
 	let response = null
-	async function sendMessageAsUser() {
+	async function sendMessageAsAdmin() {
 		response = await axios.post("/api/messages", {
 			to: "2",
 			message: "How are you doing?"
 		}, { params })
 	}
-	sendMessageAsUser().then(() => {
-		expect(response.data).toBe("Message sent")
-	},console.log)
+	await sendMessageAsAdmin()
+	expect(response.data).toBe("Message sent")
 })
 
-
-test("Read your messages as a user", () => {
+test("Read your messages as a user", async () => {
 	let response = null
 	async function sendMessagesAsUser() {
 		await axios.post("/api/messages", {
@@ -82,7 +72,7 @@ test("Read your messages as a user", () => {
 
 		response = await axios.get("/api/messages", "", { params })
 	}
-	sendMessagesAsUser().then(() => {
-		expect(response.data.length).toBe(2)
-	},console.log)
+
+	await sendMessagesAsUser()
+	expect(response.data.length).toBe(2)
 })
